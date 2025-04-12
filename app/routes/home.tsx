8c369145774c5082e6cf683c0f8f0da5369cc425
aisyahilma <aisyahilmaderajat@gmail.com>
2025-04-12 10:40:52 +0800

    Add product types and client loader for fetching products in Home component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { log } from "console";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -11,7 +12,37 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+type Product = {
+  id: number;
+  name: string;
+  slug: string;
+  description?: string | null;
+  price: number;
+  srockQuantity?: number | null;
+  isOrganic?: boolean | null;
+  weight: number;
+  imageUrl: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+type Products = Product[];
+
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  const response = await fetch(`http://localhost:3000/products`);
+  const products = (Products = await response.json());
+  return products;
+}
+
+export function HydrateFallback() {
+  return <div>Loading products...</div>;
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const products = loaderData;
+
+  console.log({ products });
+
   return (
     <div>
       <h1>Cerdiq Food</h1>
