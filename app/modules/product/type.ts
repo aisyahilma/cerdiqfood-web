cc078c46b5c58dacd2a098b2fef11a7bcf922de5
aisyahilma <aisyahilmaderajat@gmail.com>
2025-04-27 11:40:24 +0800

    Add cart route and page, implement session management for cart access, and define Zod schemas for product validation

--- app/modules/product/type.ts
+++ app/modules/product/type.ts
@@ -1,15 +1,31 @@
-export type Product = {
-  id: number;
-  name: string;
-  slug: string;
-  description?: string | null;
-  price: number;
-  stockQuantity?: number | null;
-  isOrganic?: boolean | null;
-  weight: number;
-  imageUrl: string;
-  createdAt: Date;
-  updatedAt: Date;
-};
+import { z } from "zod";
 
+export const ProductSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  slug: z.string(),
+  description: z.string().nullable().optional(),
+  price: z.number(),
+  stockQuantity: z.number().nullable().optional(),
+  isOrganic: z.boolean().nullable().optional(),
+  weight: z.number(),
+  imageUrl: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+export const ProductsSchema = z.array(ProductSchema);
+
+export const CreateProductSchema = ProductSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+}).extend({
+  slug: z.string().optional(), // slug opsional saat create
+});
+
+export const CreateProductsSchema = z.array(CreateProductSchema);
+
+// Untuk typescript types
+export type Product = z.infer<typeof ProductSchema>;
 export type Products = Product[];
